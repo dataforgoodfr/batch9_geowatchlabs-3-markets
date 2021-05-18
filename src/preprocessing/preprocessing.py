f3aea7355a4c74c7f89f1cb08f084b9debb55db7
Camille Modeste <camille.modeste@gmail.com>
2021-05-18 23:23:52 +0200

    typo: formatting

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -8,8 +8,10 @@ from pathlib import Path
 from config.preprocessing import preprocessing_FSMS_files_with_yields_types
 
 
-def preprocess_FSMS_files_with_yields(path_to_file = Path.home()+"/aggregated_match_for_FSMS_files_with_yields.csv"):
-    """ Change types, drop unnecessary columns, etc... to work with the data.
+def preprocess_FSMS_files_with_yields(
+    path_to_file=Path.home() + "/aggregated_match_for_FSMS_files_with_yields.csv",
+):
+    """Change types, drop unnecessary columns, etc... to work with the data.
 
     Args:
         path_to_file (str): path to the file
@@ -21,8 +23,10 @@ def preprocess_FSMS_files_with_yields(path_to_file = Path.home()+"/aggregated_ma
     df_aggregated_file.drop(columns=["Unnamed: 0"], inplace=True)
     columns = []
     for col in df_aggregated_file.columns.to_list():
-        new_col = col.replace("$","")
+        new_col = col.replace("$", "")
         columns.append(new_col)
     df_aggregated_file.columns = columns
-    df_aggregated_file = df_aggregated_file.astype(preprocessing_FSMS_files_with_yields_types)
+    df_aggregated_file = df_aggregated_file.astype(
+        preprocessing_FSMS_files_with_yields_types
+    )
     return df_aggregated_file
