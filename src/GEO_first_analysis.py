a2c25f65460fe38bbeddd29d12b20a2a66080c10
Camille Modeste <camille.modeste@gmail.com>
2021-05-18 23:07:03 +0200

    chore: repo restructuration

--- src/GEO_first_analysis.py
+++ src/GEO_first_analysis.py
@@ -10,33 +10,33 @@ import re, osgeo
 import matplotlib.pyplot as plt
 import pyreadstat
 
-from Auxiliary_function_for_geo_files import *
+from import_functions.auxiliary_function_for_geo_files import *
 
 # list of all files
 files = df_geo_file()
 
-# select one specific file 
-data_2010_groundnut = files[(files['crop'] == 'groundnut') &
-                            (files['year'] == '2010')].reset_index(drop=True)
+# select one specific file
+data_2010_groundnut = files[
+    (files["crop"] == "groundnut") & (files["year"] == "2010")
+].reset_index(drop=True)
 
 file = data_2010_groundnut.geomap_file[4]
 
-#read the file
+# read the file
 arr = read_geo_file(file)
-
 
 
 from osgeo import gdal, ogr, osr
 
-raster=file
-ds=gdal.Open(raster)
+raster = file
+ds = gdal.Open(raster)
 
-ext=GetExtent(ds)
+ext = get_extent(ds)
 
-src_srs=osr.SpatialReference()
+src_srs = osr.SpatialReference()
 src_srs.ImportFromWkt(ds.GetProjection())
-#tgt_srs=osr.SpatialReference()
-#tgt_srs.ImportFromEPSG(4326)
+# tgt_srs=osr.SpatialReference()
+# tgt_srs.ImportFromEPSG(4326)
 tgt_srs = src_srs.CloneGeogCS()
 
-geo_ext=ReprojectCoords(ext, src_srs, tgt_srs)
+geo_ext = reproject_coords(ext, src_srs, tgt_srs)
--- src/first_analysis.py
+++ src/first_analysis.py
@@ -1,58 +1,73 @@
 # -*- coding: utf-8 -*-
 
-
+import os
 from pathlib import Path
 import zipfile
 import pandas as pd
 import numpy as np
 import numbers
 import re
-%matplotlib inline
 import matplotlib.pyplot as plt
 import pyreadstat
+
 
 def read_data_file(num):
 
     home = str(Path.home())
 
-    zip_file = home + '/Mauritania FSMS data.zip'
-    root_folder = home + '/Mauritania FSMS data'
-    
+    zip_file = home + "/Mauritania FSMS data.zip"
+    root_folder = home + "/Mauritania FSMS data"
+
     # unzip file
     if not os.path.exists(root_folder):
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            zip_ref.extractall(home)  
-  
+        with zipfile.ZipFile(zip_file, "r") as zip_ref:
+            zip_ref.extractall(home)
+
     # list all files in folder
     list_all_files = []
     for path, subdirs, files in os.walk(root_folder):
         for name in files:
             list_all_files.append(os.path.join(path, name))
-    
+
     # list data files
-    list_data_file = [f for f in list_all_files if re.search('.sav$', f)]
-    
+    list_data_file = [f for f in list_all_files if re.search(".sav$", f)]
+
     # read data
-    data, meta =  pyreadstat.read_sav(list_data_file[num], apply_value_formats=True)
-    
-    return(data, meta)
+    data, meta = pyreadstat.read_sav(list_data_file[num], apply_value_formats=True)
+
+    return (data, meta)
+
 
 data, meta = read_data_file(0)
 
 # columns description
-columns = pd.DataFrame({'variable':meta.column_names, 'label':meta.column_labels})
+columns = pd.DataFrame({"variable": meta.column_names, "label": meta.column_labels})
 
 # rename columns
-fakedata = data.rename(columns = {'IDENT' : 'ID',
-                                  'DATE' : 'TIME' })
+fakedata = data.rename(columns={"IDENT": "ID", "DATE": "TIME"})
 # column used as identifier
-columns_id = ['Q_10_NUMQUEST', 'IDENT', 'ENQU', 'Hors_NK', 'Q_12_Moughataa', '13_Commune',
-              'VILLAG0', 'VILLAGE', 'Q_15_MILIEU', 'NUMEN', 'DATE',
-              'CDATSAISIE', 'CODE_ENQ', 'CODE_CONT', ]
-    
+columns_id = [
+    "Q_10_NUMQUEST",
+    "IDENT",
+    "ENQU",
+    "Hors_NK",
+    "Q_12_Moughataa",
+    "13_Commune",
+    "VILLAG0",
+    "VILLAGE",
+    "Q_15_MILIEU",
+    "NUMEN",
+    "DATE",
+    "CDATSAISIE",
+    "CODE_ENQ",
+    "CODE_CONT",
+]
+
 # variable of interest
 columns_value = [col for col in data.columns if col not in columns_id]
-columns_value = ['FCS', 'CSI']
+columns_value = ["FCS", "CSI"]
 
 df = pd.melt(data, id_vars=columns_id, value_vars=columns_value)
-df = df.merge(columns, on = 'variable', how='left')
+df = df.merge(columns, on="variable", how="left")
+
+
